2a663747c5f6594ab5321d9dfdb48655d4a820e1
Damien BRUNSTEIN <dbrunstein@unistra.fr>
2023-06-06 18:37:46 +0200

    converting french into english in devices.c/create_frame

--- devices.c
+++ devices.c
@@ -85,19 +85,19 @@ void create_frame(frame *frame, uint64_t src, uint64_t dest, uint8_t const *data
     }
         //trame->data;
     unsigned char *data_temp = NULL;
-    int taille = trame->type;
-    int tailleBourrage = 46; //1454
-    if(trame->type>=1454){
-        tailleBourrage = tailleBourrage-(trame->type-1454); //obtient la taille du bourrage
+    int size = frame->type;
+    int sizeFiller = 46; //1454
+    if(frame->type>=1454){
+        sizeFiller = sizeFiller-(frame->type-1454); //obtient la taille du bourrage
     }
-    data_temp = malloc(sizeof(unsigned char) * (trame->type+tailleBourrage)); // data temporaire
-    for(int i=0;i<taille;i++){ // met le message dans le data temporaire
+    data_temp = malloc(sizeof(unsigned char) * (frame->type+sizeFiller)); // data temporaire
+    for(int i=0;i<size;i++){ // met le message dans le data temporaire
         data_temp[i]=data[i]
     }
-    for(int i=taille-1;i<(trame->type+tailleBourrage);i++){ // effectue le bourrage du reste
+    for(int i=size-1;i<(frame->type+sizeFiller);i++){ // effectue le bourrage du reste
         data_temp[i]=0;
     }
-    trame->data=data_temp;
+    frame->data=data_temp;
 }
 void show_trame(TRAME *trame){
     printf("««««« TRAME »»»»»\n");
