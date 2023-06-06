ef83695f3bb17abbdff4ebe5f7ebc2712b2407e7
Damien BRUNSTEIN <dbrunstein@unistra.fr>
2023-06-06 17:57:43 +0200

    v1 create_trame avec bourrage (non testé et probablement cassé)

--- trame.c
+++ trame.c
@@ -18,7 +18,22 @@ void create_trame(TRAME *trame,unsigned char *src, unsigned char *dest,unsigned 
     trame->source=src;
     trame->destination=dest;
     trame->type=strlen(data);
-    trame->data;
+    //trame->data;
+    unsigned char *data_temp = NULL;
+    int taille = trame->type;
+    int tailleBourrage = 46; //1454
+    if(trame->type>=1454){
+        tailleBourrage = tailleBourrage-(trame->type-1454); //obtient la taille du bourrage
+    }
+    data_temp = malloc(sizeof(unsigned char) * (trame->type+tailleBourrage)); // data temporaire
+    for(int i=0;i<taille;i++){ // met le message dans le data temporaire
+        data_temp[i]=data[i]
+    }
+    for(int i=taille-1;i<(trame->type+tailleBourrage);i++){ // effectue le bourrage du reste
+        data_temp[i]=0;
+    }
+    trame->data=data_temp;
+
 }
 void show_trame(TRAME *trame){
     printf("««««« TRAME »»»»»\n");
